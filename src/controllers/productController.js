f2d6360caeed5e523e9bf40c4c5d04edf71ecb1e
mahtoaman <itismyacnt@gmail.com>
2022-10-12 16:22:58 +0530

    udpated product added, need to work on this not functioning

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -8,6 +8,7 @@ const {
   isValid,
   isValidObjectId,
   isValidImg,
+  isValidName,
 } = require("../validators/validation");
 
 const createProducts = async (req, res) => {
@@ -95,12 +96,10 @@ const createProducts = async (req, res) => {
         .send({ status: false, message: "Please Enter Currency Symbol" });
     }
     if (currencyFormat != "₹") {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: "Currency Symbol should be only in '₹'",
-        });
+      return res.status(400).send({
+        status: false,
+        message: "Currency Symbol should be only in '₹'",
+      });
     }
 
     //isFreeShipping validation
@@ -111,12 +110,10 @@ const createProducts = async (req, res) => {
           isFreeShipping.toLowerCase() === "false"
         )
       ) {
-        return res
-          .status(400)
-          .send({
-            status: false,
-            message: "Please Provide only Boolean Value",
-          });
+        return res.status(400).send({
+          status: false,
+          message: "Please Provide only Boolean Value",
+        });
       }
       data["isFreeShipping"] = isFreeShipping.toLowerCase();
     }
@@ -197,4 +194,166 @@ const createProducts = async (req, res) => {
   }
 };
 
-module.exports = { createProducts };
+//=====================================UPDATE PRODUCT========================================
+
+const updateProducts = async (req, res) => {
+  try {
+    let data = req.body;
+    let userId = req.params.userId
+    let file = req.files;
+
+    let {
+      title,
+      description,
+      price,
+      currencyId,
+      currencyFormat,
+      isFreeShipping,
+      availableSizes,
+      style,
+      installments,
+    } = data;
+
+    if (!isValidRequestBody(data)) {
+      return res.status(400).send({
+        status: false,
+        message: "Provide the Product's Data",
+      });
+    }
+
+    let updateProduct = {};
+
+    if (title) {
+      if (!isValidName(title))
+        return res
+          .status(400)
+          .send({ status: false, message: "Title is invalid" });
+
+      let checkTitle = await productModel.findOne({ title: title });
+      if (checkTitle)
+        return res.status(400).send({
+          status: false,
+          message: `Product with this ${title} is Already Present`,
+        });
+      updateProduct["title"] = title;
+    }
+
+    if (description) {
+      if (!isValid(description))
+        return res.status(400).send({
+          status: false,
+          message: "Please Write Description About Product ",
+        });
+      updateProduct["description"] = description;
+    }
+
+    if (price) {
+      if (!/^[0-9]*$/.test(price))
+        return res
+          .status(400)
+          .send({ status: false, message: "Price should be in Number" });
+      updateProduct["price"] = price;
+    }
+
+    // if (currencyId) {
+    //   if (currencyId != "INR")
+    //     return res
+    //       .status(400)
+    //       .send({ status: false, message: "CurrencyId should be in INR" });
+    // updateProduct['currencyId'] = currencyId
+    // }
+
+    // if (currencyFormat != "₹") {
+    //   return res.status(400).send({
+    //     status: false,
+    //     message: "Currency Symbol should be only in '₹'s",
+    //   });
+    // }
+
+    //isFreeShipping validation
+    if (isFreeShipping) {
+      if (
+        !(
+          isFreeShipping.toLowerCase() === "true" ||
+          isFreeShipping.toLowerCase() === "false"
+        )
+      ) {
+        return res.status(400).send({
+          status: false,
+          message: "Please Provide only Boolean Value",
+        });
+      }
+      data["isFreeShipping"] = isFreeShipping.toLowerCase();
+    }
+
+    if (file > 0) {
+      if (!isValidImg(file[0].mimetype)) {
+        return res.status(400).send({
+          status: false,
+          message: "Image Should be of JPEG/ JPG/ PNG",
+        });
+        let url = await aws1.uploadFile(file[0]);
+        updateProduct["productImage"] = url;
+      }
+      let url = await aws1.uploadFile(file[0]);
+      data["productImage"] = url;
+    }
+    // if (availableSizes) {
+    //   return res
+    //     .status(400)
+    //     .send({ status: false, message: "Please Enter Size of Product" });
+    // }
+
+    // let sizeArr = availableSizes.replace(/\s+/g, "").split(",").map(String);
+    // let arr = ["S", "XS", "M", "X", "L", "XXL", "XL"];
+    // let flag;
+    // for (let i = 0; i < sizeArr.length; i++) {
+    //   flag = arr.includes(sizeArr[i]);
+    // }
+    // if (flag == false) {
+    //   return res.status(400).send({
+    //     status: false,
+    //     data: "Enter a Valid Size, Like 'XS or S or M or X or L or XL or XXL'",
+    //   });
+    //   updateProduct["availableSizes"] = sizeArr;
+    // }
+
+    if (installments) {
+      if (!/^[0-9]*$/.test(installments)) {
+        return res.status(400).send({
+          status: false,
+          message: "Installments value Should be only number",
+        });
+      }
+      if (installments < 0) {
+        return res.status(400).send({
+          status: false,
+          message: "installments Shoud be In Valid  Number only",
+        });
+      }
+    updateProduct['installments'] = installments
+    }
+
+    //style validation
+    if (style != null) {
+      if (!isValid(style)) {
+        return res
+          .status(400)
+          .send({ status: false, message: "Provide the style " });
+      }
+    updateProduct['style'] = style
+    }
+
+    //after checking all the validation, than creating the product data
+    const createdProduct = await productModel.findOneAndUpdate(updateProduct);
+    return res.status(201).send({
+      status: true,
+      message: "Product is Created Successfully",
+      data: createdProduct,
+    });
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
+};
+
+module.exports = { createProducts, updateProducts };
