e033b8eb081a8ff2aae9180925e85df7b856e314
AnujTembhurne <you@example.com>
2022-10-17 17:10:14 +0530

    create

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,6 +1,6 @@
 const cartModel = require("../models/cartModel");
 const productModel = require("../controllers/productController");
-const { isValidObjectId } = require("../validators/validation");
+const { isValidObjectId,isValidRequestBody } = require("../validators/validation");
 
 const createCart = async function (req, res) {
   try {
@@ -9,16 +9,13 @@ const createCart = async function (req, res) {
     let { quantity, productId } = data;
     let cartId = req.body.cartId;
 
+    if(!isValidRequestBody(data)) return res.status(400).send({status:false,message:" please provide data"})
+
     if (!isValidObjectId(userId))
       return res
         .status(400)
         .send({ status: false, message: "Not a valid userId" });
 
-    let cartAvaiable = await findOne({ userId: userId });
-
-    if (!cartAvaiable) {
-      let cart = [{ productId: productId, quantity: 1 }];
-    }
 
     let product = await productModel.findOne({
       _id: productId,
@@ -30,16 +27,25 @@ const createCart = async function (req, res) {
         .status(404)
         .send({ status: false, message: "Product not found" });
 
-    let pPrice = product.price;
+    let cartAvaiable = await cartModel.findOne({ _id:cartId });
+    if(cartAvaiable){
+        let cart = {productId:productId,quantity:quantity}
+        cartAvaiable.items.push(cart)
+        cartAvaiable.totalItems= items.length
+    }
 
-    let finalCart = {
-      userId: userId,
-      items: cart,
-      totalPrice: pPrice * quantity,
-      totalItems: items.length,
-    };
-
+    if (!cartAvaiable) {
+        let pPrice = product.price;
+        let cart = {productId:productId,quantity:quantity}
+        var finalCart = {
+          userId: userId,
+          items: cart,
+          totalPrice: pPrice * quantity,
+          totalItems: items.length,
+        }
+    }
     let createCart = await cartModel.create(finalCart);
+    return res.status(201).send({status:false,message:"cart successfully created",data:createCart})
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
